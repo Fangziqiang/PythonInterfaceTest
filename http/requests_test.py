4a39dc8af9d92e2d3ee27f4949574e65566d17fe
fangziqiang <286330540@qq.com>
2019-07-29 17:18:42 +0800

    11111

--- http/requests_test.py
+++ http/requests_test.py
@@ -1,17 +1,24 @@
-'''
-Created on 2019年3月20日
-
-@author: Administrator
-'''
+#coding=utf-8
 import requests
 # from pip._vendor.requests.auth import HTTPBasicAuth
 
-print (requests.get('http://localhost:8081/jenkins/api/json?pretty=true',auth=('admin','admin')).text)
+# 多个参数格式：
+# {"key1":"value1","key2":"value2","key3":"value3"}
+yoyo_url='http://www.cnblogs.com/yoyoketang/'
+blog_search_url='http://zzk.cnblogs.com/s/blogpost'
+
+par={"Keywords":"youyouketang"}
+r=requests.get(blog_search_url,params=par)
+
+print r.text
+r.status_code
+
+# print (requests.get('http://localhost:8081/jenkins/api/json?pretty=true',auth=('admin','admin')).text)
 
 
-url='http://localhost:8081/jenkins/job/see_python_version/build'
-# r=requests.post(url,data={},auth=HTTPBasicAuth('admin','admin'))
-r=requests.post(url,data={},auth=('admin','admin'))
-print (r.status_code)
-print (r.headers)
-print (r.reason)
+# url='http://localhost:8081/jenkins/job/see_python_version/build'
+# # r=requests.post(url,data={},auth=HTTPBasicAuth('admin','admin'))
+# r=requests.post(url,data={},auth=('admin','admin'))
+# print (r.status_code)
+# print (r.headers)
+# print (r.reason)
