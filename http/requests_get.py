4ce32e34107e4b878010cd15d5f47a9bd63a8322
fangziqiang <286330540@qq.com>
2019-07-30 17:54:04 +0800

    1111111

--- http/requests_get.py
+++ http/requests_get.py
@@ -6,12 +6,26 @@ import requests
 # {"key1":"value1","key2":"value2","key3":"value3"}
 yoyo_url='http://www.cnblogs.com/yoyoketang/'
 blog_search_url='http://zzk.cnblogs.com/s/blogpost'
+baidu_url='http://www.baidu.com'
 
 par={"Keywords":"youyouketang"}
-r=requests.get(blog_search_url,params=par)
 
-print r.text
-r.status_code
+#请求博客搜索页
+# r=requests.get(blog_search_url,params=par)
+
+#请求百度首页
+r=requests.get(baidu_url)
+
+print r.url
+print r.encoding    #编码格式
+print r.content     #获取返回内容，这个方法会自动解码gzip和deflate压缩
+print r.headers
+print r.cookies
+# response的其他返回内容
+print r.status_code       #响应状态码
+print r.raw               #返回原始响应体
+print r.json()            #Requests中内置的JSON解码器
+print r.raise_for_status()  #失败请求抛出异常（非200响应）
 
 # print (requests.get('http://localhost:8081/jenkins/api/json?pretty=true',auth=('admin','admin')).text)
 
